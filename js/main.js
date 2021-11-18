063060e7b86f843ad7bb809a7f173b4d728d113b
mivlev1 <makzoyn@gmail.com>
2021-11-18 21:02:24 +0300

    edit js

--- js/main.js
+++ js/main.js
@@ -10,75 +10,39 @@ openPopUp.addEventListener('click', function (e) {
 closePopUp.addEventListener('click', () => {
   popUp.classList.remove('active');
 })
-
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('form');
-  form.addEventListener('submit', formSend);
-
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-
-    let formData = new FormData(form);
-    
-    if (error === 0) {
-      let response = await fetch('https://formcarry.com/s/Ger4-7rnaWH', {
-        method: 'POST',
-        body: formData
+      $(function () {
+        $(".ajaxForm").submit(function (e) {
+          e.preventDefault();
+          var href = $(this).attr("action");
+          $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function (response) {
+              if (response.status == "success") {
+                alert("Ваше обращение получено, спасибо!");
+                form.reset();
+              } else {
+                alert("Ошибка отправки: " + response.message);
+              }
+            }
+          });
+        });
       });
-
-      if (response.ok) {
-        let result = await response.json();
-        alert(result.message);
-        form.reset();
-      }
-      else {
-        alert("Ошибка");
-      }
+});
+$(window).on('hashchange', function (event) { //при клике на "назад" скрывать форму
+  let popOverlay = g
+  if (window.location.hash == "#popUp") {
+    $popOverlay.fadeIn();
+    $popupMainWindow.fadeIn();
+  }
+  else {
+    if (window.location.hash != "#popUp") {
+      $popOverlay.fadeOut();
+      $popupMainWindow.fadeOut();
+      $popThankYouWindow.fadeOut();
     }
-    else {
-      alert("Заполните обязательные поля");
-    }
-
-  }
-  function formValidate(form) {
-    let error = 0;
-    let formReque = document.querySelector('._reque')
-
-    for (let i = 0; i < formReque.length; i++) {
-      const input = formReque[i];
-      removeError(input);
-      if(input.classList.contains('_email')){
-        if (emailTest(input)){
-          formAddError(input);
-          error++;
-        }
-        else if(input.getAttribute("type")==="checkbox" && input.checked === false){
-          addError(input);
-          error++;
-        }
-        else
-        {
-          if(input.value=== ' '){
-            addError(input);
-            error++;
-          }
-        }
-      }
-    }
-    return error;
-  }
-  function addError(input) {
-    input.parentElement.classList.add('_error')
-    input.classList.add('_error')
-  }
-  function removeError(input) {
-    input.parentElement.classList.remove('_error')
-    input.classList.remove('_error')
-  }
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]&\w+)*(\.\w{2,8})+$/.test(input.value);
-    
   }
 });
