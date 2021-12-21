e0c9f3f4bcbdd9b597bf465be337034587663cd3
mivlev1 <makzoyn@gmail.com>
2021-12-21 23:01:17 +0300

    edit js and add historyAPI

--- js/main.js
+++ js/main.js
@@ -1,48 +1,42 @@
-const openPopUp = document.getElementById('open_popUp');
+
+const openPopUp = document.querySelector('.open__popUp');
 const closePopUp = document.getElementById('popUp__close');
 const popUp = document.getElementById('popUp');
+const popUpBody = document.querySelector('.popup__body');
+const popUpContainer = document.querySelector('.popup__container');
 
-openPopUp.addEventListener('click', function (e) {
-  e.preventDefault();
-  popUp.classList.add('active');
-  
-})
-closePopUp.addEventListener('click', () => {
-  popUp.classList.remove('active');
-})
-document.addEventListener('DOMContentLoaded', function () {
-      $(function () {
-        $(".ajaxForm").submit(function (e) {
-          e.preventDefault();
-          var href = $(this).attr("action");
-          $.ajax({
-            type: "POST",
-            dataType: "json",
-            url: href,
-            data: $(this).serialize(),
-            success: function (response) {
-              if (response.status == "success") {
-                alert("Ваше обращение получено, спасибо!");
-                form.reset();
-              } else {
-                alert("Ошибка отправки: " + response.message);
-              }
-            }
-          });
-        });
+document.addEventListener('DOMContentLoaded', () => {
+  openPopUp.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.onpopstate = function () {
+      window.history.back();
+      popUp.classList.remove('active');
+    };
+    history.pushState({ page: 1 }, 'pop', '?pop');
+    popUp.classList.add('active');
+  });
+  closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+    history.back();
+  })
+  $(function () {
+    $(".ajaxForm").submit(function (e) {
+      e.preventDefault();
+      var href = $(this).attr("action");
+      $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: href,
+        data: $(this).serialize(),
+        success: function (response) {
+          if (response.status == "success") {
+            alert("Ваше обращение получено, спасибо!");
+            form.reset();
+          } else {
+            alert("Ошибка отправки: " + response.message);
+          }
+        }
       });
-});
-$(window).on('hashchange', function (event) { //при клике на "назад" скрывать форму
-  let popOverlay = g
-  if (window.location.hash == "#popUp") {
-    $popOverlay.fadeIn();
-    $popupMainWindow.fadeIn();
-  }
-  else {
-    if (window.location.hash != "#popUp") {
-      $popOverlay.fadeOut();
-      $popupMainWindow.fadeOut();
-      $popThankYouWindow.fadeOut();
-    }
-  }
+    });
+  });
 });
