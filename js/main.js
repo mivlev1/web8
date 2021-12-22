7453c36bc38195f68e1ee5a21328a39227819ed3
Maxim Ivlev <86892885+mivlev1@users.noreply.github.com>
2021-12-22 17:32:06 +0300

    edit js

--- js/main.js
+++ js/main.js
@@ -8,16 +8,18 @@ const popUpContainer = document.querySelector('.popup__container');
 document.addEventListener('DOMContentLoaded', () => {
   openPopUp.addEventListener('click', (e) => {
     e.preventDefault();
+    /*
+    Управление историей стрелками в браузере
     window.onpopstate = function () {
       window.history.back();
-      popUp.classList.remove('active');
-    };
+       popUp.classList.remove('active');
+     };*/
     history.pushState(null, null, 'pop');
     popUp.classList.add('active');
   });
   closePopUp.addEventListener('click', () => {
+    history.back();
     popUp.classList.remove('active');
-    //history.back();
   })
   $(function () {
     $(".ajaxForm").submit(function (e) {
@@ -32,6 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
           if (response.status == "success") {
             alert("Ваше обращение получено, спасибо!");
             form.reset();
+            history.back();
+            popUp.classList.remove('active');
           } else {
             alert("Ошибка отправки: " + response.message);
           }
